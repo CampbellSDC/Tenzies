6f37978d65b5f870cbf1e3755916da68b98d6c41
Steve <scampbellatc@gmail.com>
2023-07-31 19:19:23 -0400

    added Die component

---
+++ Tenzies/src/Components/Die.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Die(){
+    return (
+        <>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        <div className="die-box"></div>
+        </>
+    )
+}
