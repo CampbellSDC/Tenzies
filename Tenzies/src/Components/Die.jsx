9ad5c97d111f90dab7d4f8862ce82533be6f4bd4
Steve <scampbellatc@gmail.com>
2023-08-03 12:47:28 -0400

    updated held die styling

--- Tenzies/src/Components/Die.jsx
+++ Tenzies/src/Components/Die.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export default function Die(props){
+    // * The style variable is set up to do conditional style rendering 
+    // * base on if the isHeld is true or false
+    const style = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
     return (
         
         <>
-            <div className="die-box">
+            <div className="die-box" style={style}>
 
                 <h2 className="die-num">{props.value}</h2>
             </div>
