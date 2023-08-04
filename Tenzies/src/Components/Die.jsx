73c32b81122095cd9bdfface7aeaacd2d8c68a8e
Steve <scampbellatc@gmail.com>
2023-08-04 10:49:40 -0400

    started holdDice function

--- Tenzies/src/Components/Die.jsx
+++ Tenzies/src/Components/Die.jsx
@@ -9,7 +9,7 @@ export default function Die(props){
     return (
         
         <>
-            <div className="die-box" style={style}>
+            <div onClick={props.holdDice} className="die-box" style={style}>
 
                 <h2 className="die-num">{props.value}</h2>
             </div>
